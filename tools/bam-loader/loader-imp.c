4989b31ea493b07c8e6ab629a372c0a2c0d68dcb
kdurbrow <durbrowk@ncbi.nlm.nih.gov>
2016-11-15 15:46:36 -0500

    a slight refactor

--- tools/bam-loader/loader-imp.c
+++ tools/bam-loader/loader-imp.c
@@ -1761,9 +1761,9 @@ MIXED_BASE_AND_COLOR:
             }
             memset(value, 0, sizeof(*value));
             value->unmated = !mated;
+            value->pcr_dup = (flags & BAMFlags_IsDuplicate) == 0 ? 0 : 1;
+            value->platform = GetINSDCPlatform(bam, spotGroup);
             if (isPrimary || G.assembleWithSecondary || G.deferSecondary) {
-                value->pcr_dup = (flags & BAMFlags_IsDuplicate) == 0 ? 0 : 1;
-                value->platform = GetINSDCPlatform(bam, spotGroup);
                 value->primary_is_set = 1;
             }
         }
